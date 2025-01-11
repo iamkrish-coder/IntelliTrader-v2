cefec67b9a9ca92cc2dcd2afd0c5607592124e7d
Srikrishnan P <krishnan.sri92@gmail.com>
2025-01-11 17:31:46 +0530

    Fix: Update release workflow and remove verification_token table

--- backend/database/definitions/verification_token.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS verification_token
-(
-    identifier TEXT NOT NULL,
-    expires TIMESTAMPTZ NOT NULL,
-    token TEXT NOT NULL,
-
-    PRIMARY KEY (identifier, token)
-); 
